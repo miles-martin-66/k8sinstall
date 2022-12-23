5c7f6f141b2dc9ce9c213b48e447810cbd953a19
root <root@debian.localdomain>
2022-12-23 20:19:09 +0000

    Adjusted the timeout check for metallb services from 90 to 300 seconds.

--- k8s-rpi4-install.sh
+++ k8s-rpi4-install.sh
@@ -95,7 +95,7 @@ cp calicoctl /usr/local/bin/
 kubectl taint node debian node-role.kubernetes.io/control-plane-
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
-kubectl wait --namespace metallb-system --for=condition=ready pod --selector=app=metallb --timeout=90s
+kubectl wait --namespace metallb-system --for=condition=ready pod --selector=app=metallb --timeout=300s
 cat <<EOF | sudo tee ./metallb.yaml
 apiVersion: metallb.io/v1beta1
 kind: IPAddressPool
